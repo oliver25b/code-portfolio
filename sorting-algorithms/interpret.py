5877a03a58467345194cdda5318912b1c7def06e
oliver25b <112600682+oliver25b@users.noreply.github.com>
2023-01-24 15:32:12 -0500

    fixed axis labels, broke legend

--- sorting-algorithms/interpret.py
+++ sorting-algorithms/interpret.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('data.csv', index_col=False)
 
 print(data)
+
+plt.plot(data)
+
+plt.legend()
+plt.yscale('log')
+plt.ylabel('Time to Sort (ms)')
+plt.xticks(ticks=range(0,10), labels=data.iloc[:,0])
+plt.xlabel('Number of Sortees')
+plt.show()
